ff0a7b28db2f1acb2b1afa994fd25ced2861a59d
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-09-19 15:42:22 -0700

    add additional info and styling

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { WeatherContext } from "../../hooks/weatherContext";
 import conditionsData from "./conditionsData";
+import WbTwilightRoundedIcon from "@mui/icons-material/WbTwilightRounded";
+import OpacityRoundedIcon from "@mui/icons-material/OpacityRounded";
 import "./WeatherInfoStyles.scss";
 
 const WeatherInfo = () => {
@@ -31,11 +33,50 @@ const WeatherInfo = () => {
           </div>
           <div className="info-header">{formattedCity}</div>
           <p>{weatherData.weather[0].description}</p>
+          <div className="info-row-wrapper">
+            <div className="info-row-item">
+              <div className="info-row-hero adjust">
+                {Math.round(weatherData.main.temp_min)}°
+              </div>
+              <div className="info-row-secondary">min</div>
+            </div>
+            <div className="info-row-item">
+              <div className="info-row-hero adjust">
+                {Math.round(weatherData.main.temp_max)}°
+              </div>
+              <div className="info-row-secondary">max</div>
+            </div>
+            <div className="info-row-item">
+              <div className="info-row-hero">
+                <WbTwilightRoundedIcon />
+              </div>
+              <div className="info-row-secondary">
+                {convertToPST(weatherData.sys.sunrise)}
+              </div>
+            </div>
+            <div className="info-row-item">
+              <div className="info-row-hero">
+                <WbTwilightRoundedIcon />
+              </div>
+              <div className="info-row-secondary">
+                {convertToPST(weatherData.sys.sunset)}
+              </div>
+            </div>
+            <div className="info-row-item">
+              <div className="info-row-hero">
+                <OpacityRoundedIcon />
+              </div>
+              <div className="info-row-secondary">
+                {weatherData.main.humidity}
+              </div>
+            </div>
+          </div>
+          {/* <p>{weatherData.weather[0].description}</p>
           <p>humidity: {weatherData.main.humidity}</p>
           <p>min: {weatherData.main.temp_min}</p>
           <p>max: {weatherData.main.temp_max}</p>
           <p>sunrise: {convertToPST(weatherData.sys.sunrise)}</p>
-          <p>sunrise: {convertToPST(weatherData.sys.sunset)}</p>
+          <p>sunrise: {convertToPST(weatherData.sys.sunset)}</p> */}
         </div>
       ) : null}
     </div>
