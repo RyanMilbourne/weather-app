5eb05d91f565e1b515f9ba2dd39907ed76e6a972
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-09-23 11:11:52 -0700

    remove template pages

--- src/routes/HomeRoute.jsx
+++ src/routes/HomeRoute.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../sections/homepage/Homepage";
 import useScrollToTop from "../hooks/useScrollToTop";
-import About from "../sections/about/About";
-import Contact from "../sections/contact/Contact";
 
 const HomeRoute = () => {
   useScrollToTop();
@@ -11,8 +9,6 @@ const HomeRoute = () => {
     <section className="content-section">
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Homepage />} />
       </Routes>
     </section>
